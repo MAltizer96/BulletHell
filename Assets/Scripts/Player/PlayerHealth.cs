fe2c30e8bdc8c1f2a02088999d071f9c1e8de3f1
MAltizer96 <Maltizer96@gmail.com>
2026-07-16 22:18:06 -0400

    Changed name from Health.cs to PlayerHealth.cs

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] healthIcons; // Array of health icon GameObjects
