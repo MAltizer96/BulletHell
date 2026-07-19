00f1dfb967f5306f11f8e11273e174684f6ac7bb
MAltizer96 <Maltizer96@gmail.com>
2026-07-18 22:01:28 -0400

    Update GameManager.cs
    -- added event for tracking if player dies

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,46 @@
-using UnityEngine;
+using System;
 using TMPro;
+using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private float playedTimer = 0f;
 
     private TextMeshProUGUI timerText;
 
+    private SpawnManager spawnManager;
+
+    void OnEnable()
+    {
+        PlayerHealth.OnPlayerDied += HandlePlayerDied;
+    }
+
+    void OnDisable()
+    {
+        PlayerHealth.OnPlayerDied -= HandlePlayerDied;
+    }
+
     private void Awake()
     {
         timerText = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
     }
     private void Update()
     {
         playedTimer += Time.deltaTime;
         timerText.text = "Time Played: " + Mathf.FloorToInt(playedTimer).ToString() + " seconds";
 
+    }
+
+    void HandlePlayerDied(PlayerHealth player)
+    {
+        Debug.Log("Player has died. Game Over!");
+        // Implement game over logic here, such as showing a game over screen or restarting the level.
+
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = false; // Disable player movement
+            spawnManager.SpawnEnemies = false; // Stop spawning enemies
+        }
     }
 }
