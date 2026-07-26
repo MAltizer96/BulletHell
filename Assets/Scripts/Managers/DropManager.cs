172c9cf09b79f5302ae2298db5d7d4bcd64eed9a
MAltizer96 <Maltizer96@gmail.com>
2026-07-25 23:08:18 -0400

    Update DropManager.cs
    - added gunDropChance & healthDropChance variable
    - changed chance of drop

--- Assets/Scripts/Managers/DropManager.cs
+++ Assets/Scripts/Managers/DropManager.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class DropManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] gunPickupPrefab;
-    [SerializeField] private GameObject healthPickupPrefab;
-    [SerializeField] private float gunDropChance = 0.1f;
-    [SerializeField] private float healthDropChance = 0.2f;
+    [SerializeField] 
+    private GameObject[] gunPickupPrefab;
+    [SerializeField] 
+    private GameObject healthPickupPrefab;
+    [SerializeField] 
+    private float baseGunDropChance = 0.1f;
+    [SerializeField] 
+    private float baseHealthDropChance = 0.2f;
+
+    [SerializeField]
+    float gunDropChance;
+    [SerializeField]
+    float healthDropChance;
 
     void OnEnable()
     {
@@ -17,41 +26,39 @@ public class DropManager : MonoBehaviour
     {
         Enemy.OnEnemyDied -= HandleEnemyDied;
     }
-    private void Awake()
-    {
-        Enemy.OnEnemyDied += HandleEnemyDied;
-    }
+
     void SpawnPickup(GameObject pickUpToSpawn, Transform location)
     {
         // spawn that bitch in!
         pickUpToSpawn = Instantiate(pickUpToSpawn, location.position, Quaternion.identity);
     }
+
     void HandleEnemyDied(Enemy enemy)
     {
         if (Random.value <= gunDropChance)
         {
             GameObject pickup = gunPickupPrefab[Random.Range(0, gunPickupPrefab.Length)];
             SpawnPickup(pickup, enemy.transform);
-            gunDropChance = 0.05f; // Reset the chance for the next drop
+            gunDropChance = baseGunDropChance; // Reset the chance for the next drop
+            return;
         }
         else
         {
             Debug.Log("No pickup dropped.");
             gunDropChance += 0.01f; // Increase the chance for the next drop
         }
-
         if (Random.value <= healthDropChance)
         {
             Debug.Log("Health pickup dropped.");
             
             SpawnPickup(healthPickupPrefab, enemy.transform);
 
-            healthDropChance = 0.05f; // Reset the chance for the next drop
+            healthDropChance = baseHealthDropChance; // Reset the chance for the next drop
         }
         else
         {
             Debug.Log("No health pickup dropped.");
-            healthDropChance += 0.075f; // Increase the chance for the next drop
+            healthDropChance += 0.015f; // Increase the chance for the next drop
         }
     }
 }
